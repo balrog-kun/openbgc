23c298d2174c8ffb02b98f00b36eed6b3dac49d1
Andrew Zaborowski <balrogg+code@gmail.com>
2025-10-21 02:15:22 +0200

    mpu6050: Handle FIFO update race and improve overflow handling
    First, account for the start of the FIFO being misaligned with the start
    of the first sample in the FIFO after an overflow.  This is what was
    screwing the AHRS up badly ever since I added the FIFO support for gyro
    readings because the axes were effectively swapped.
    
    Second, handle the (really easy to trigger) race condition between the
    read and the MPU60x0 updating its FIFO registers.  This is easy but gets
    complicated when combined with the FIFO overflow situation so the
    handling of these two scenarios together is the difficult part here.
    
    With these two improvements it's finally possible to compare AHRS
    performance with and without the FIFO, and unfortunately the difference
    isn't noticeable.  The performance is still poor where any movement of
    around 90 degrees introduces a 6-8 degree error (if only fast enough
    that the accelerometer Kp doesn't allow the accelerometer corrections to
    catch up).  The hope is that it's going to be easier to find the source
    of this error after an UI is added to visualise the real vs. estimated
    orientations, which in any case would happen after a PoC level is
    reached in actual camera stabilization.  And if that doesn't help then
    more data collection and simulation will have to happen as the AHRS
    precision is quite high priority.  It's at least 10x worse right now
    than it should be.
    
    Also make sure when there are no new samples we return the last sample
    instead of 0's (in non-blocking mode).

--- src/imu-mpu6050.cpp
+++ src/imu-mpu6050.cpp
@@ -83,18 +83,22 @@ static void mpu6050_free(struct mpu6050_s *dev) {
 
 static void mpu6050_read_main(struct mpu6050_s *dev, int32_t *accel, int32_t *gyro) {
 #ifdef USE_FIFO
-    uint16_t byte_cnt = 0, sample_cnt;
+    uint16_t byte_cnt = 0;
+    uint8_t sample_cnt;
     int32_t gyro_total[3] = {};
-# define DATA_SIZE 8
+
+    uint8_t reg_data_size = 8;
+# define FIFO_SAMPLE_SIZE 6
+    bool fifo_failsafe = false;
 #else
-# define DATA_SIZE 14
+    uint8_t reg_data_size = 14;
 #endif
 
-    uint8_t buffer[DATA_SIZE];
+    uint8_t buffer[14];
 
 #ifdef USE_FIFO
 # ifdef BLOCK_UNTIL_NEW_DATA
-    while (gyro && byte_cnt < SAMPLE_SIZE) {
+    while (gyro && byte_cnt < FIFO_SAMPLE_SIZE) {
 # else
     if (gyro) {
 # endif
@@ -106,14 +110,88 @@ static void mpu6050_read_main(struct mpu6050_s *dev, int32_t *accel, int32_t *gy
         byte_cnt = (uint16_t) dev->i2c->read() << 8;
         byte_cnt |= dev->i2c->read();
     }
+
+    sample_cnt = byte_cnt / FIFO_SAMPLE_SIZE;
+    if (byte_cnt > sample_cnt * FIFO_SAMPLE_SIZE) {
+        /* This may happen for two reasons, separately or both at once:
+         *
+         * If the FIFO has overflowed (FIFO_COUNT returned 1024), there will be
+         * 1024 % FIFO_SAMPLE_SIZE bytes at the beginning of the FIFO belonging to a
+         * partially overwritten sample and we'll need to discard these bytes to align with
+         * the initial bytes of the rest of the samples in the FIFO.
+         *
+         * Separately it seems that FIFO_COUNT is not updated atomically and even without an
+         * overflow may return a value not divisible by FIFO_SAMPLE_SIZE (and not even
+         * divisble by 2).  In that case we must not discard any bytes from the beginning
+         * because the samples are not offset/misaligned -- the partial sample is
+         * (momentarily) at the end of the FIFO.  But we need to leave the last
+         * FIFO_COUNT % FIFO_SAMPLE_SIZE bytes in the FIFO for a future read (or wait and
+         * retry).
+         *
+         * The real question, though, is whether the MPU60x0's internal FIFO start (think of
+         * a pointer into a ring buffer) is updated atomically -- it probably isn't.  And if
+         * it isn't then we have no reliable strategy to handle the overflowed FIFO.  On
+         * overflow, FIFO_COUNT will always read 1024 but the samples inside the FIFO may be
+         * offset from the beginning by a value that's neither 1024 % FIFO_SAMPLE_SIZE nor 0,
+         * and we cannot know.
+         *
+         * Conceivably, even if the FIFO hasn't overflowed at the time we read FIFO_COUNT,
+         * it may have been in the process of writing the sample that will cause the oveflow
+         * by the time we read FIFO_R_W and we're also screwed.
+         *
+         * Consequently handle this scenario by emptying the FIFO and returning only the
+         * last sample from the DATA_START area.  Separately do check whether the gyro
+         * readings are nonsense in all the places where we expect we may have an overflow,
+         * which is in the calibration code.
+         */
+        byte_cnt = sample_cnt * FIFO_SAMPLE_SIZE;
+    }
+
+    if (byte_cnt >= 1024 - FIFO_SAMPLE_SIZE || !sample_cnt) {
+        reg_data_size += FIFO_SAMPLE_SIZE;
+        fifo_failsafe = true;
+
+        /* Recover by emptying the FIFO.
+         *
+         * FIFO_RESET in USER_CTRL register might also work but in tests it didn't seem to help in
+         * this scenario, neither with nor without the wait for FIFO_RESET to become 0.
+         */
+        while (byte_cnt) {
+            uint8_t byte_cnt_chunk = min((int) byte_cnt, 255);
+
+            byte_cnt -= byte_cnt_chunk;
+            if (dev->i2c->requestFrom(dev->i2c_addr, byte_cnt_chunk, MPU6050_REG_FIFO_R_W, 1, true) !=
+                    byte_cnt_chunk) {
+                /* TODO: report error */
+                return;
+            }
+
+            if (byte_cnt)
+                continue;
+
+            /* Re-read FIFO_COUNT.  If we've read the number of bytes it reported before,
+             * it should have gone done to 0.  If it hasn't gone up by a single byte over
+             * the time it took us to read the buffer then the MPU60x0 probably wasn't in
+             * the middle of an update and we can assume the FIFO start is aligned with
+             * the beginning of a sample.  Fingers crossed.
+             */
+            if (dev->i2c->requestFrom(dev->i2c_addr, (uint8_t) 2, MPU6050_REG_FIFO_COUNT, 1, true) != 2) {
+                /* TODO: report error */
+                return;
+            }
+
+            byte_cnt = (uint16_t) dev->i2c->read() << 8;
+            byte_cnt |= dev->i2c->read();
+        }
+    }
 #endif
 
-    if (dev->i2c->requestFrom(dev->i2c_addr, (uint8_t) DATA_SIZE, MPU6050_REG_DATA_START, 1, true) != DATA_SIZE) {
+    if (dev->i2c->requestFrom(dev->i2c_addr, reg_data_size, MPU6050_REG_DATA_START, 1, true) != reg_data_size) {
         /* TODO: report error */
         return;
     }
 
-    for (uint8_t i = 0; i < DATA_SIZE; i++)
+    for (uint8_t i = 0; i < reg_data_size; i++)
         buffer[i] = dev->i2c->read();
 
     /* Accelerometer data */
@@ -131,23 +209,15 @@ static void mpu6050_read_main(struct mpu6050_s *dev, int32_t *accel, int32_t *gy
 
     /* Gyroscope data */
 #ifdef USE_FIFO
-    byte_cnt = min((int) byte_cnt, 1024);
-
-# define SAMPLE_SIZE 6
-    sample_cnt = byte_cnt / SAMPLE_SIZE;
-    if (!sample_cnt) {
-        gyro[0] = 0;
-        gyro[1] = 0;
-        gyro[2] = 0;
-        /* TODO: return previous reading? */
-        return;
-    }
+    if (fifo_failsafe)
+        /* Return previous reading if no data and non-blocking.  Not ideal.  TODO: error reporting */
+        goto failsafe;
 
     for (uint8_t i = 0; i < sample_cnt;) {
-        uint8_t sample_cnt_chunk = min((int) sample_cnt - i, 255 / SAMPLE_SIZE);
+        uint8_t sample_cnt_chunk = min((int) sample_cnt - i, 255 / FIFO_SAMPLE_SIZE);
 
         i += sample_cnt_chunk;
-        byte_cnt = sample_cnt_chunk * SAMPLE_SIZE;
+        byte_cnt = sample_cnt_chunk * FIFO_SAMPLE_SIZE;
 
         if (dev->i2c->requestFrom(dev->i2c_addr, byte_cnt, MPU6050_REG_FIFO_R_W, 1, true) != byte_cnt) {
             /* TODO: report error */
@@ -164,28 +234,36 @@ static void mpu6050_read_main(struct mpu6050_s *dev, int32_t *accel, int32_t *gy
         }
     }
 
-    uint16_t c = sample_cnt / 2; /* Compensate for the rounding down when dividing by sample_cnt */
-    if (gyro_total[0] > 0)
-        gyro_total[0] += c;
-    else
-        gyro_total[0] -= c;
-    if (gyro_total[1] > 0)
-        gyro_total[1] += c;
-    else
-        gyro_total[1] -= c;
-    if (gyro_total[2] > 0)
-        gyro_total[2] += c;
-    else
-        gyro_total[2] -= c;
+    {
+        uint16_t c = sample_cnt / 2; /* Compensate for the rounding down when dividing by sample_cnt */
+        if (gyro_total[0] > 0)
+            gyro_total[0] += c;
+        else
+            gyro_total[0] -= c;
+        if (gyro_total[1] > 0)
+            gyro_total[1] += c;
+        else
+            gyro_total[1] -= c;
+        if (gyro_total[2] > 0)
+            gyro_total[2] += c;
+        else
+            gyro_total[2] -= c;
+    }
 
+    /* Note: could shift this and mpu6050_imu_class.gyro_scale left by a few bits to avoid
+     * the rounding errors even more.  However even at current scale the noise seems to be
+     * way over 1 LSB.
+     */
     gyro[0] = gyro_total[0] / sample_cnt;
     gyro[1] = gyro_total[1] / sample_cnt;
     gyro[2] = gyro_total[2] / sample_cnt;
-#else
+    return;
+
+failsafe:
+#endif
     gyro[0] = (int16_t) (buffer[8] << 8 | buffer[9]);
     gyro[1] = (int16_t) (buffer[10] << 8 | buffer[11]);
     gyro[2] = (int16_t) (buffer[12] << 8 | buffer[13]);
-#endif
 }
 
 static void mpu6050_read_temp(struct mpu6050_s *dev, int32_t *temp) {
